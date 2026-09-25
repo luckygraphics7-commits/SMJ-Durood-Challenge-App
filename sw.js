15bc3c204ffcc8e3111eb1f533ab5e21ad2f5fc5
luckygraphics7-commits <lucky.graphics7@gmail.com>
2026-09-25 17:16:26 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='smj-durood-v3-v3';
-const ASSETS=['./','./index.html','./manifest.json','./assets/logo.png','./assets/madina.jpg'];
+const ASSETS=['./','./index.html','./manifest.json','logo.png','madina.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));

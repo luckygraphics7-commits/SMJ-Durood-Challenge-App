39e77bf4cfbc239bbba761ad7f21f3190010945d
luckygraphics7-commits <lucky.graphics7@gmail.com>
2026-09-25 23:25:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='smj-durood-v3-v3';
-const ASSETS=['./','./index.html','./manifest.json','logo.png','madina.jpg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='smj-durood-v7';
+const ASSETS=['./','./index.html','./manifest.json','./logo.png','./madina.jpg','./icon-192.png','./icon-512.png','./favicon-64.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
